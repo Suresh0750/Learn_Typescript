f4f8f46083a7399612322bf53f7cad11c4c9bd7c
Suresh0750 <sureshvarma0750@gmail.com>
2024-06-21 06:48:18 -0700

    checked all data-types

---
+++ practice.js
@@ -0,0 +1,28 @@
+var sample = 'Hari';
+sample = 'suresh'; //# we can't assign number
+// console.log(sample)
+var subscribe = 0;
+// subscribe = 'suresh' 
+subscribe = 21;
+// console.log(subscribe)
+var arr = ['str', 'str1', 'str3'];
+arr = ["0"];
+// console.log(arr)
+var checkBol = true;
+checkBol = false;
+// console.log(checkBol)
+var checkBolArr = [true, false, true];
+// checkBolArr = [0,true] // # its wrong
+// console.log(checkBolArr)
+var emptyArr = [];
+// emptyArr = [1,2,3]
+// console.log(typeof(emptyArr))
+// console.log(emptyArr)
+// # object 
+var obj = {};
+console.log(obj);
+var obj1 = {
+    name: 'suresh',
+    age: 21,
+    isMarried: true
+};
--- practice.ts
+++ practice.ts
@@ -1,5 +1,46 @@
 
-let sample = 'Hari'
+let sample : string = 'Hari'
+
+sample = 'suresh' //# we can't assign number
+// console.log(sample)
+
+let subscribe:number = 0
+
+// subscribe = 'suresh' 
+subscribe = 21
+// console.log(subscribe)
+
+let arr : string[] = ['str','str1','str3']
+
+arr = ["0"]
+
+// console.log(arr)
+let checkBol :boolean = true
+
+checkBol = false
+// console.log(checkBol)
+
+let checkBolArr : boolean[] = [true,false,true]
+
+// checkBolArr = [0,true] // # its wrong
+// console.log(checkBolArr)
+
+let emptyArr : [] = []
+// emptyArr = [1,2,3]
+
+// console.log(typeof(emptyArr))
+
+// console.log(emptyArr)
+
+// # object 
+let obj: {} = {}
+
+console.log(obj)
+
+let obj1:{name:string;age:number,isMarried:boolean} = {  // If we mention the property we have to give value of inside of object
+    name : 'suresh',
+    age : 21,
+    isMarried: true
+}
 
 
-console.log(sample)
